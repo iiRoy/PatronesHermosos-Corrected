8cf6c2917bce306879510b4346135370f6e394a0
Rodrigo <rock.gule@gmail.com>
2025-04-03 12:25:54 -0600

    Notifications Update

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -14,8 +14,9 @@ const User = () => {
                     </Link>
                 </div>
                 <div className="flex flex-col lg:flex-row items-center lg:items-end justify-center lg:justify-end mb-5 p-1 pt-0">
-                    <Link href={"/"} className="flex items-center justify-center p-2">
+                    <Link href={"/"} className="w-auto h-auto flex items-center justify-center p-2 cursor-pointer relative">
                         <Image src={"/message.png"} alt="Notifications" width={25} height={25} className="lg:max-w-[20px] lg:max-h-[20px] min-w-[17px] min-h-[17px]" />
+                        <div className='absolute transform w-auto h-4 top-0 right-0.5 p-1 pr-1.5 pl-1.5 flex items-center justify-center bg-purple-500 text-white rounded-full text-[calc(70%-0.2em)]'>1</div>
                     </Link>
                     <Link href={"/"} className="flex items-center justify-center p-2">
                         <Image src={"/exam.png"} alt="Example1" width={25} height={25} className="lg:max-w-[20px] lg:max-h-[20px] min-w-[17px] min-h-[17px]" />
