721b37efaa486bd04ef802305d1571c374210c81
Ale Guzman <a01736793@tec.mx>
2025-04-04 12:28:48 -0600

    Cambios componentes

---
+++ src/components/buttons_inputs/Button.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Image from "next/image";
+
+interface ButtonProps {
+    label: string;
+    variant: "primary" | "secondary" | "success" | "error" | "warning";
+    disabled?: boolean;
+    round?: boolean;
+    icon?: string;
+    onClick?: () => void;
+}
+
+const Button: React.FC<ButtonProps> = ({ label, variant, disabled, round, icon }) => {
+    const baseClass = "custom-button";
+    const variantClass = `button-${variant}`;
+    const disabledClass = disabled ? `${variant}-disabled dim` : "";
+    const roundClass = round ? "round-button" : "";
+
+    return (
+        <button className={`${baseClass} ${variantClass} ${disabledClass} ${roundClass}`}>
+            {icon ? (
+                <Image src={icon} alt="icon" width={20} height={20} className="mr-2" />
+            ) : (
+                <i className="fa-regular fa-star icon"></i>
+            )}
+            {!round && <span className="label">{label}</span>}
+            {!round && <i className="fa-solid fa-xmark close"></i>}
+        </button>
+    );
+};
+
+export default Button;
---
+++ src/components/buttons_inputs/OptionLink.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+interface OptionLinkProps {
+    label: string;
+    icon: string;
+    href: string;
+}
+
+const OptionLink: React.FC<OptionLinkProps> = ({ label, icon, href }) => {
+    const pathname = usePathname(); // Obtiene la ruta actual
+
+    return (
+        <Link href={href} className={`option-link ${pathname === href ? "active" : ""}`}>
+            <Image src={icon} alt={label} width={25} height={25} className="option-icon" />
+            <span className="option-label">{label}</span>
+        </Link>
+    );
+};
+
+export default OptionLink;
