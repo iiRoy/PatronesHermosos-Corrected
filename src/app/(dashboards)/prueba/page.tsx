d855650b6bba14092e51c5378a74bf06b4bb9119
Ale Guzman <a01736793@tec.mx>
2025-04-06 21:43:26 -0600

    Nuevos componentes

--- src/app/(dashboards)/prueba/page.tsx
+++ src/app/(dashboards)/prueba/page.tsx
@@ -5,6 +5,9 @@ import UserCard from "@/components/headers_menu_users/UserCard"
 import Navbar from "@/components/headers_menu_users/navbar";
 import Pagination from "@/components/buttons_inputs/Pagination";
 import InputField from "@/components/buttons_inputs/InputField";
+import Notification from "@/components/buttons_inputs/Notification";
+import Button from "@/components/buttons_inputs/Button";
+import Checkbox from "@/components/buttons_inputs/Checkbox";
 
 const EstadisticasAdmin = () => {
     return (
@@ -63,6 +66,91 @@ const EstadisticasAdmin = () => {
                     variant="warning"
                     iconSrc="/student.png"
                 />
+                <Notification
+                    color="green"
+                    variant="one"
+                    title="¡Éxito!"
+                    message="Tu acción se completó correctamente."
+                />
+                <Notification
+                    color="purple"
+                    variant="one"
+                    title="Notificación"
+                    message="Has recibido una notificación"
+                />
+                <Notification
+                    color="red"
+                    variant="two"
+                    title="Error"
+                    message="Hubo un problema al guardar los datos."
+                />
+                <Notification
+                    color="yellow"
+                    variant="two"
+                    title="Advertencia"
+                    message="Uno de los datos introducidos es incorrecto"
+                />
+                <Button
+                    label="Perfil"
+                    variant="secondary"
+                    showLeftIcon
+                    leftIconPath="/student.png"
+                    showRightIcon
+                    rightIconPath="/close.png"
+                />
+                <Button
+                    label="Add"
+                    variant="primary"
+                    round
+                    showLeftIcon
+                    leftIconPath="/plus.png"
+                />
+                <Button
+                    label="Enviar"
+                    variant="success"
+                />
+
+                <Checkbox
+                    label="Label"
+                    color="white"
+                    checked={true}
+                    bordered={false}
+                    onChange={(val) => console.log('New state:', val)}
+                />
+
+                <Checkbox
+                    label="Label"
+                    color="purple"
+                    checked={true}
+                    bordered={false}
+                    onChange={(val) => console.log('New state:', val)}
+                />
+
+                <Checkbox
+                    label="Label"
+                    color="green"
+                    checked={false}
+                    onChange={(val) => console.log('New state:', val)}
+                />
+
+                <Checkbox
+                    label="Label"
+                    color="yellow"
+                    checked={false}
+                    bordered={true}
+                    onChange={(val) => console.log('New state:', val)}
+                />
+
+                <Checkbox
+                    label="Label"
+                    color="red"
+                    checked={true}
+                    bordered={false}
+                    onChange={(val) => console.log('New state:', val)}
+                />
+
+
+
             </div>
         </div>
     )
--- src/components/headers_menu_users/navbar.tsx
+++ src/components/headers_menu_users/navbar.tsx
@@ -35,38 +35,38 @@ const Navbar = () => {
         <div className='flex item-center justify-between pr-4 pl-4 pb-2 pt-2 bg-[#2E1C31] shadow-md w-full rounded-b-lg'>
             {/*LOGO*/}
             <div className="hidden md:block flex items-center justify-center">
-            <Image src="/logo.png" alt="logo" width={60} height={60} className="center" />
+                <Image src="/logo.png" alt="logo" width={60} height={60} className="center" />
             </div>
             {/*NAVIGATION*/}
             <div className="">
-            {navItems.map(i => (
-                <div key={i.title} className="flex gap-2 items-center justify-center h-full">
-                {i.items.map(item => (
-                    item.type === "link" ? (
-                        <Link
-                        href={item.href}
-                        key={item.label}
-                        className="flex items-center justify-center gap-2 p-2 rounded-md transition duration-200 ease-in-out group"
-                    >
-                        <div className="center">
-                            <Image
-                                src={item.icon}
-                                alt=""
-                                width={25}
-                                height={25}
-                                className="group-hover:fill-[#B673BD] transition duration-200 ease-in-out"
-                            />
-                        </div>
-                        <span className="hidden lg:block text-white">{item.label}</span>
-                    </Link>                    
-                    ) : (
-                    <button key={item.label} className="flex items-center justify-center gap-2 p-2 rounded-md bg-blue-500 text-white hover:bg-blue-600 transition duration-200 ease-in-out">
-                        <Image src={item.icon} alt="" width={30} height={30} className="center" />
-                        <span className="hidden lg:block text-white-500">{item.label}</span>
-                    </button>
-                    )
-                ))}
-                </div>))}
+                {navItems.map(i => (
+                    <div key={i.title} className="flex gap-2 items-center justify-center h-full">
+                        {i.items.map(item => (
+                            item.type === "link" ? (
+                                <Link
+                                    href={item.href}
+                                    key={item.label}
+                                    className="flex items-center justify-center gap-2 p-2 rounded-md transition duration-200 ease-in-out group"
+                                >
+                                    <div className="center">
+                                        <Image
+                                            src={item.icon}
+                                            alt=""
+                                            width={25}
+                                            height={25}
+                                            className="group-hover:fill-[#B673BD] transition duration-200 ease-in-out"
+                                        />
+                                    </div>
+                                    <span className="hidden lg:block text-white">{item.label}</span>
+                                </Link>
+                            ) : (
+                                <button key={item.label} className="flex items-center justify-center gap-2 p-2 rounded-md bg-blue-500 text-white hover:bg-blue-600 transition duration-200 ease-in-out">
+                                    <Image src={item.icon} alt="" width={30} height={30} className="center" />
+                                    <span className="hidden lg:block text-white-500">{item.label}</span>
+                                </button>
+                            )
+                        ))}
+                    </div>))}
 
             </div>
         </div>
