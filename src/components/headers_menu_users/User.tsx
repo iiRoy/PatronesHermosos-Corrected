e094ef0ee5b4b3d490f6e474ffb923ecf9f78539
Rodrigo <rock.gule@gmail.com>
2025-04-07 10:53:20 -0600

    Cambios Visuales del Programa
    Versión 0.2.1

--- src/components/headers_menu_users/User.tsx
+++ src/components/headers_menu_users/User.tsx
@@ -8,15 +8,15 @@ const User = () => {
                     <Link href={"/"} className="flex items-center justify-center gap-3 p-2">
                         <Image src={"/avatar.png"} alt="user" width={35} height={35} className="md:min-w-[3.5vw] md:min-h-[3.5vw] min-w-[5vmax] min-h-[5vmax] rounded-full" />
                         <div className='flex flex-col text-left hidden lg:block'>
-                            <span className="text-sm leading-5 text-[1.5vmax]">María Patricia de los Ángeles</span>
-                            <span className='text-[1.05vmax] font-medium text-textDim block break-all'>@username_example</span>
+                            <span className="text-sm leading-5 text-[1.5vmax]">User</span>
+                            <span className='text-[1vmax] font-medium text-textDim block break-all'>@username_example</span>
                         </div>
                     </Link>
                 </div>
                 <div className="flex flex-col lg:flex-row items-center lg:items-end justify-center lg:justify-end pb-2 pr-1">
                     <Link href={"/"} className="w-auto h-auto flex items-center justify-center p-2 cursor-pointer relative">
                         <Image src={"/message.png"} alt="Notifications" width={25} height={25} className="md:min-w-[1vw] md:min-h-[1vw] min-w-[3vmax] min-h-[3vmax]" />
-                        <div className='absolute transform w-[0.9vw] h-[0.9vw] md:min-w-[1.5vw] md:min-h-[1.5vw] min-w-[2vmax] min-h-[2vmax] top-0 right-0.5 p-1 pr-1.5 pl-1.5 flex items-center justify-center bg-purple-500 text-white rounded-full text-[clamp(0.5rem,1.0vw,2rem)]'>1</div>
+                        <div className='absolute transform -translate-y-1/2 w-[0.9vw] h-[0.9vw] md:min-w-[1.5vw] md:min-h-[1.5vw] min-w-[2.2vmax] min-h-[2.2vmax] top-2 lg:top-1.5 lg:right-0 right-1 p-1 pr-1.5 pl-1.5 flex items-center justify-center bg-purple-500 text-white rounded-full text-[clamp(0.5rem,1.0vw,2rem)]'>1</div>
                     </Link>
                     <Link href={"/"} className="flex items-center justify-center p-2">
                         <Image src={"/exam.png"} alt="Example1" width={25} height={25} className="md:min-w-[1vw] md:min-h-[1vw] min-w-[3vmax] min-h-[3vmax]" />
