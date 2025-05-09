2ebe9ea3fd7802c1f64cdbe387fb404e3c2c151a
FMaggo <a01736935@tec.mx>
2025-05-09 14:11:36 -0600

    fix(page): Resolved Bugs/Removal of unnecessary Token

--- src/server.js
+++ src/server.js
@@ -22,6 +22,7 @@ const PORT = process.env.PORT || 3000;
 
 appNext.prepare().then(() => {
   // Middlewares globales
+  app.set('trust proxy', 1);
   app.use(express.json({ limit: '50mb' })); // Increase JSON payload limit
   app.use(express.urlencoded({ limit: '50mb', extended: true })); // Optional: for form-urlencoded data
   app.use(morgan('dev'));
