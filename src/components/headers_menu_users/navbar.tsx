dabffa5cfac8f5a0e811639963c458307d661b83
Rodrigo López <rock.gule@gmail.com>
2025-05-12 11:12:31 -0600

    chore(unfinished): PDF Files

--- src/components/headers_menu_users/navbar.tsx
+++ src/components/headers_menu_users/navbar.tsx
@@ -2,8 +2,10 @@
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import OptionLink from '../buttons_inputs/OptionLink';
 import * as Icons from '../icons';
+import Button from '../buttons_inputs/Button';
 
 const navItems = [
   {
@@ -24,6 +26,7 @@ const navItems = [
 ];
 
 const Navbar = () => {
+  const router = useRouter();
   return (
     <div className='min-h-[45px] flex item-center justify-between pr-4 pl-4 pb-2 pt-2 w-full rounded-b-lg'>
       {/* Logo */}
@@ -43,7 +46,7 @@ const Navbar = () => {
       {/* Navegación */}
       <div className='text-[clamp(1rem,1.5vw,3rem)] items-center flex gap-[1.5vmax]'>
         {navItems.map((section) => (
-          <div key={section.title} className='flex gap-[1.5vmax] px-2'>
+          <div key={section.title} className='flex gap-[3vmax] px-2'>
             {section.items.map((item) => {
               const IconComponent = Icons[item.icon as keyof typeof Icons];
               return (
@@ -55,6 +58,7 @@ const Navbar = () => {
                 />
               );
             })}
+            <Button label='Iniciar Sesión' variant="secondary" showLeftIcon IconLeft={Icons.FingerprintSimple} onClick={() => router.push('/login')} activeTransition={true} />
           </div>
         ))}
       </div>
