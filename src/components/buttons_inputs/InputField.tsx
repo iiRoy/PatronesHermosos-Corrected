89c89db8b8a687a318445352a83d2f395591e344
Rodrigo López <rock.gule@gmail.com>
2025-05-25 20:51:43 -0600

    style(components): Some components style fixes

--- src/components/buttons_inputs/InputField.tsx
+++ src/components/buttons_inputs/InputField.tsx
@@ -61,7 +61,7 @@ const InputField: React.FC<InputFieldProps> = ({
     }
   };
 
-  const inputClass = `input input-${variant}${disabled ? ' input-disabled' : ''}${darkText ? ' darkText' : ''}`;
+  const inputClass = `input input-${variant}${disabled ? ' input-disabled' : ''}${darkText ? ' darkText' : ''} transition-all duration-500 ease-in-out`;
   const errorClass =
     variant === 'warning' || variant.includes('warning') ? 'error-text-red' : 'error-text';
 
