4d3825f6913424ce7a0e0821fee758cff729c1b3
Rodrigo López <rock.gule@gmail.com>
2025-05-27 09:54:55 -0600

    chore(estadísticas): Base de gráfica lineal

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -58,6 +58,7 @@ export default function LoginLayout({
   }
 
   return (
+<<<<<<< Updated upstream
     <div className='h-screen w-screen flex flex-col text-text bg-[#160D17]'>
       <div className='h-[13%] min-h-[70px] shadow-custom-dark w-full flex items-center justify-between bg-[var(--background)] rounded-br-[1.5vmax] rounded-bl-[1.5vmax] z-10 overflow-hidden'>
         <Navbar />
@@ -68,14 +69,26 @@ export default function LoginLayout({
     transition-opacity duration-500 ease-in-out
     ${isVisible ? 'opacity-100' : 'opacity-0'}
   `}>
+=======
+    <div className='h-screen w-screen flex flex-col text-text overflow-hidden bg-[#160D17]'>
+      <div className='h-[13%] min-h-[70px] shadow-custom-dark w-full flex items-center justify-between bg-[var(--background)] rounded-br-[1.5vmax] rounded-bl-[1.5vmax] z-10'>
+        <Navbar />
+      </div>
+            {/* Círculos decorativos */}
+      <div className='absolute mt-[50px] inset-0'>
+>>>>>>> Stashed changes
         <div className='absolute top-[10vw] left-[-2vw] w-[6vw] h-[6vw] bg-[#C57FAB] rounded-full -translate-y-1/2 shadow-custom-dark'></div>
         <div className='absolute top-0 left-[3vw] w-[10vw] h-[10vw] bg-[#97639C] rounded-full translate-x-1/3 -translate-y-1/4 shadow-custom-dark'></div>
         <div className='absolute top-[4vw] left-[-4vw] w-[9vw] h-[9vw] bg-[#2E1C31] rounded-full translate-x-1/4 -translate-y-1/2 shadow-custom-dark'></div>
         <div className='absolute top-[6vw] left-[16vw] w-[4vw] h-[4vw] bg-[#EBE6EB] rounded-full shadow-custom-dark'></div>
         <div className='absolute top-[9vw] left-[5vw] w-[2.5vw] h-[2.5vw] bg-[#EBE6EB] rounded-full shadow-custom-dark'></div>
       </div>
+<<<<<<< Updated upstream
       <div className={`absolute inset-0 overflow-clip transition-opacity duration-500 ease-in-out
     ${isVisible ? 'opacity-100' : 'opacity-0'}`}>
+=======
+      <div className='absolute inset-0 overflow-clip'>
+>>>>>>> Stashed changes
         <div className='absolute bottom-[-7vw] right-[2vw] w-[10vw] h-[10vw] bg-[#C57FAB] rounded-full -translate-y-1/2 shadow-custom-dark'></div>
         <div className='absolute bottom-[-5vw] right-[15vw] w-[7vw] h-[7vw] bg-[#C57FAB] rounded-full -translate-y-1/2 shadow-custom-dark'></div>
         <div className='absolute bottom-[-6vw] right-[12vw] w-[8vw] h-[8vw] bg-[#97639C] rounded-full translate-x-1/3 -translate-y-1/4 shadow-custom-dark'></div>
@@ -84,7 +97,11 @@ export default function LoginLayout({
         <div className='absolute bottom-[3vw] right-[27vw] w-[3.5vw] h-[3.5vw] bg-[#EBE6EB] rounded-full shadow-custom-dark'></div>
         <div className='absolute bottom-[6vw] right-[13vw] w-[2vw] h-[2vw] bg-[#EBE6EB] rounded-full shadow-custom-dark'></div>
       </div>
+<<<<<<< Updated upstream
       <div className='h-fit w-full flex items-center justify-center align-middle relative z-0 overflow-y-visible overflow-x-hidden custom-scrollbar'>
+=======
+      <div className='h-[87%] w-full flex items-center justify-center relative z-0 overflow-hidden'>
+>>>>>>> Stashed changes
         <div className='flex flex-col justify-center items-center w-full h-full'>{children}</div>
       </div>
     </div>
