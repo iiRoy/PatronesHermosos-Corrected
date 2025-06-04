83127b5034ec7847630bad9dde762266c2f2b9cc
Ale Guzman <a01736793@tec.mx>
2025-06-04 14:21:58 -0600

    improve(pages): página de editar sede con diseño corregido superuser

--- src/app/(private)/(dashboards)/admin/sedes/page.tsx
+++ src/app/(private)/(dashboards)/admin/sedes/page.tsx
@@ -13,6 +13,8 @@ import { useNotification } from '@/components/buttons_inputs/Notification';
 interface Sede {
     id_venue: number;
     name: string;
+    estado: string;
+    pais: string;
     location: string;
     address: string;
     status: string;
@@ -255,7 +257,7 @@ const SedesAdmin = () => {
                                 <th className="p-2 text-center">Nombre</th>
                                 <th className="p-2 text-center">Ubicación</th>
                                 <th className="p-2 text-center">Dirección</th>
-                                <th className="p-2 text-center">Status</th>
+                                <th className="p-2 text-center">Estado</th>
                                 <th className="p-2 text-center"></th>
                             </tr>
                         </thead>
@@ -313,7 +315,7 @@ const SedesAdmin = () => {
                                 <p><strong>Nombre:</strong> {selectedSede.name}</p>
                                 <p><strong>Ubicación:</strong> {selectedSede.location}</p>
                                 <p><strong>Dirección:</strong> {selectedSede.address}</p>
-                                <p><strong>Status:</strong> {selectedSede.status}</p>
+                                <p><strong>Estado:</strong> {selectedSede.status}</p>
                             </div>
                             <div className="mt-6 flex justify-center">
                                 <Button
