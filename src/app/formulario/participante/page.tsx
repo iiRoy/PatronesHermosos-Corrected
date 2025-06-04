8dfd3adc1d53a30b0a71ba72037dcc2edf67d6dd
FMaggo <a01736935@tec.mx>
2025-06-03 19:10:07 -0600

    fix(forms): All forms now use proper button type to prevent early submission/email sent when creating venue

--- src/app/formulario/participante/page.tsx
+++ src/app/formulario/participante/page.tsx
@@ -247,6 +247,7 @@ const ParticipantRegistrationForm: React.FC = () => {
             label=""
             variant="error"
             round
+            type='button'
             showLeftIcon
             IconLeft={X}
             onClick={() => router.push('/inicio')}
@@ -367,6 +368,28 @@ const ParticipantRegistrationForm: React.FC = () => {
           />
         </div>
 
+        {/* Selección de Grupo */}
+        <div className="mb-6">
+          <h2 className="text-xl md:text-2xl font-semibold flex items-center mb-2">
+            <span className="mr-2"><AddressBook /></span> Selección de Grupo
+          </h2>
+          <p className="text-gray-400 text-sm md:text-base mb-4">
+            Selecciona el grupo que prefieres. Usa los botones para más detalles.
+          </p>
+        </div>
+
+        <div className="flex justify-center items-center mx-auto w-[80%] h-[50px] rounded-t-[15px] mt-8 bg-gray-700 text-xs sm:text-base md:text-lg lg:text-xl">
+          <h2 className="mx-4 font-semibold">Grupo Elegido: </h2>
+          <p>{groups.find(g => g.id_group === formData.preferred_group)?.name || 'Ninguno'}</p>
+        </div>
+        <ParticipantGroupSelectionTable
+          onSelect={(id_group) => {
+            handleGroupSelect(id_group);
+          }}
+          selectedGroupId={formData.preferred_group ?? undefined}
+          rowsPerPage={4}
+        />
+
         {/* Permiso de Participación */}
         <div className="mt-8">
           <h2 className="text-xl md:text-2xl font-semibold flex items-center mb-2">
@@ -411,34 +434,13 @@ const ParticipantRegistrationForm: React.FC = () => {
           </div>
         </div>
 
-        {/* Selección de Grupo */}
-        <div className="mb-6">
-          <h2 className="text-xl md:text-2xl font-semibold flex items-center mb-2">
-            <span className="mr-2"><AddressBook /></span> Selección de Grupo
-          </h2>
-          <p className="text-gray-400 text-sm md:text-base mb-4">
-            Selecciona el grupo que prefieres. Usa los botones para más detalles.
-          </p>
-        </div>
-
-        <div className="flex justify-center items-center mx-auto w-[80%] h-[50px] rounded-t-[15px] mt-8 bg-gray-700 text-xs sm:text-base md:text-lg lg:text-xl">
-          <h2 className="mx-4 font-semibold">Grupo Elegido: </h2>
-          <p>{groups.find(g => g.id_group === formData.preferred_group)?.name || 'Ninguno'}</p>
-        </div>
-        <ParticipantGroupSelectionTable
-          onSelect={(id_group) => {
-            handleGroupSelect(id_group);
-          }}
-          selectedGroupId={formData.preferred_group ?? undefined}
-          rowsPerPage={4}
-        />
-
         {/* Submit Button */}
         <div className="mt-6 flex justify-end">
           <Button
             label="Enviar Registro"
             variant="success"
             showRightIcon
+            type='submit'
             IconRight={withIconDecorator(Send)}
             className="px-6 py-2 rounded-full flex items-center"
           />
