68094f58434e9e136c51a2d71058363d5be249f3
Ale Guzman <a01736793@tec.mx>
2025-06-04 12:04:22 -0600

    improve(pages): páginas de editar usuarios en coordinadora con diseño mejorado

--- src/app/(private)/(dashboards)/coordinador/gestion-usuarios-coordinadora/staff/page.tsx
+++ src/app/(private)/(dashboards)/coordinador/gestion-usuarios-coordinadora/staff/page.tsx
@@ -383,7 +383,7 @@ const GestionApoyo = () => {
                 )}
                 {isInfoPopupOpen && selectedApoyo && (
                     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-                        <div className="bg-white p-6 rounded-lg shadow-lg w-96 max-h-[80vh] overflow-y-auto text-gray-800">
+                        <div className="bg-white p-6 rounded-lg shadow-lg w-96 max-h-[80vh] overflow-y-auto text-gray-800 custom-scrollbar-tabla">
                             <h2 className="text-3xl font-bold mb-4 text-center">Información del Colaborador</h2>
                             <div className="space-y-2">
                                 <p><strong>Nombre:</strong> {selectedApoyo.name}</p>
